754cea5768d1d992b142ea05b97400cc520c712a
Heleir <itzelvargas2002@gmail.com>
2023-05-20 16:53:35 -0500

    Inicialización de pokemart

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#header').load('../html/header.html', function (){
+        $('#search').on("mouseover", function () {
+            console.log("hover");
+            $('#searchbar').focus();
+        }).on("mouseout", function(){
+            $('#searchbar').blur();
+        });
+        console.log("Cargó header");
+    });
+    
+    $('#footer').load('../html/footer.html', function(){
+        console.log("Cargó footer");
+    });
+});
