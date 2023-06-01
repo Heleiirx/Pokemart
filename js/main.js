e304c5ae47712d3c4aa9457a3eeec5aa8c24f42c
Heleir <itzelvargas2002@gmail.com>
2023-05-31 23:17:20 -0500

    Adicion de side bar filtros

--- js/main.js
+++ js/main.js
@@ -13,3 +13,48 @@ $(function(){
         console.log("Cargó footer");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+        const toggle = document.getElementById(toggleId);
+        console.log("Toggle declarado:::->" + toggle);
+        nav = document.getElementById(navId);
+        console.log("nav declarado:::->" + nav);
+        bodypd = document.getElementById(bodyId);
+        console.log("bodypd declarado:::->" + bodypd);
+        headerpd = document.getElementById(headerId);
+        console.log("headerpd declarado:::->" + headerpd);
+        
+        // Validate that all variables exist
+        if(toggle && nav && bodypd && headerpd){
+            toggle.addEventListener('click', ()=>{
+                console.log("click en filtro");
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+                console.log("terminó funcion click");
+            })
+        }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header-catalogo')
+    
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+        if(linkColor){
+            linkColor.forEach(l=> l.classList.remove('active'));
+            this.classList.add('active');
+        }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+    // Your code to run since DOM is loaded and ready
+});
